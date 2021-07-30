a7a1090e1026c5bff8d75f2fac4d892be2429495
Mózes Hajdú <Mozes.Hajdu@ibm.com>
2021-07-30 09:50:30 +0200

    scenario player picks cell extended impl

--- src/main/java/player/Player.java
+++ src/main/java/player/Player.java
@@ -3,10 +3,12 @@ package player;
 import board.Cell;
 
 import java.util.List;
+import java.util.Random;
 
 public class Player {
 
     public Cell pickCell(List<Cell> availableCells) {
-        return availableCells.get(0);
+        int randomIndex = new Random().nextInt(availableCells.size());
+        return availableCells.get(randomIndex);
     }
 }
