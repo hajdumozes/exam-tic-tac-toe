d581c858d8f9971c3e9dcfb8f91449b0d1f7cc78
Mózes Hajdú <Mozes.Hajdu@ibm.com>
2021-07-30 09:56:01 +0200

    game manager class added

---
+++ src/main/java/game/GameManager.java
@@ -0,0 +1,16 @@
+package game;
+
+import board.GameBoard;
+import board.Occupier;
+import lombok.Getter;
+import player.Player;
+
+import java.util.List;
+
+@Getter
+public class GameManager {
+    GameBoard gameBoard;
+    List<Player> players = List.of(new Player(Occupier.PLAYER_X), new Player(Occupier.PLAYER_O));
+    Player currentPlayer = players.get(0);
+    boolean isGameOver;
+}
--- src/main/java/player/Player.java
+++ src/main/java/player/Player.java
@@ -1,11 +1,20 @@
 package player;
 
 import board.Cell;
+import board.Occupier;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
 
 import java.util.List;
 import java.util.Random;
 
+@Getter
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class Player {
+    Occupier occupier;
 
     public Cell pickCell(List<Cell> availableCells) {
         int randomIndex = new Random().nextInt(availableCells.size());
