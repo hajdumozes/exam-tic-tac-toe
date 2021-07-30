5ade4a897e4066d368d8b809a13afb229de3375b
Mózes Hajdú <Mozes.Hajdu@ibm.com>
2021-07-30 10:42:29 +0200

    scenario vertical win case 3 green

--- src/main/java/game/GameOverDecider.java
+++ src/main/java/game/GameOverDecider.java
@@ -20,7 +20,7 @@ public class GameOverDecider {
     public boolean isItVerticalWin(List<Cell> cells) {
         List<Cell> firstColumn = List.of(new Cell(0, 0), new Cell(1, 0), new Cell(2, 0));
         List<Cell> secondColumn = List.of(new Cell(0, 1), new Cell(1, 1), new Cell(2, 1));
-        List<Cell> thirdLine = List.of(new Cell(2, 0), new Cell(2, 1), new Cell(2, 2));
-        return cells.containsAll(firstColumn) || cells.containsAll(secondColumn) || cells.containsAll(thirdLine);
+        List<Cell> thirdColumn = List.of(new Cell(0, 2), new Cell(1, 2), new Cell(2, 2));
+        return cells.containsAll(firstColumn) || cells.containsAll(secondColumn) || cells.containsAll(thirdColumn);
     }
 }
