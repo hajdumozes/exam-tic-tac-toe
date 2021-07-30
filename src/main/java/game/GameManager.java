ad652fc8691e8048d8b3e2762789fa7c54f53e56
Mózes Hajdú <Mozes.Hajdu@ibm.com>
2021-07-30 10:11:42 +0200

    GameManager refactor

--- src/main/java/game/GameManager.java
+++ src/main/java/game/GameManager.java
@@ -2,15 +2,18 @@ package game;
 
 import board.GameBoard;
 import board.Occupier;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.experimental.FieldDefaults;
 import player.Player;
 
 import java.util.List;
 
 @Getter
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class GameManager {
-    GameBoard gameBoard = new GameBoard();
-    List<Player> players = List.of(new Player(Occupier.PLAYER_X), new Player(Occupier.PLAYER_O));
+    final GameBoard gameBoard = new GameBoard();
+    final List<Player> players = List.of(new Player(Occupier.PLAYER_X), new Player(Occupier.PLAYER_O));
     Player currentPlayer = players.get(0);
     boolean isGameOver = false;
 
